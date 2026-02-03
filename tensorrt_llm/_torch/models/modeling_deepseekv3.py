e1304150521e8b6e59c00fcfa2f4f16b4714ca1c
shicli <shicli@nvidia.com>
2026-02-03 15:51:49 +0800

    fix

--- tensorrt_llm/_torch/models/modeling_deepseekv3.py
+++ tensorrt_llm/_torch/models/modeling_deepseekv3.py
@@ -1638,11 +1638,13 @@ class DeepseekV3ForCausalLM(SpecDecOneEngineForCausalLM[DeepseekV3Model,
                     layer.mlp.shared_experts = None
 
         # Also process MTP layers if present
-        for layer in self.model.layers[self.config.num_hidden_layers:]:
-            # MTP layers also have MoE, need to fuse shared experts
-            if hasattr(layer, 'mlp') and hasattr(layer.mlp, 'experts'):
-                if hasattr(layer.mlp.experts, 'num_fused_shared_expert'
-                           ) and layer.mlp.experts.num_fused_shared_expert > 0:
-                    layer.mlp.experts.fuse_shared_expert(
-                        layer.mlp.shared_experts)
-                    layer.mlp.shared_experts = None
+        if self.draft_model is not None and hasattr(self.draft_model,
+                                                    'mtp_layers'):
+            for layer in self.model.layers[self.config.num_hidden_layers:]:
+                # MTP layers also have MoE, need to fuse shared experts
+                if hasattr(layer, 'mlp') and hasattr(layer.mlp, 'experts'):
+                    if hasattr(layer.mlp.experts, 'num_fused_shared_expert'
+                               ) and layer.mlp.experts.num_fused_shared_expert > 0:
+                        layer.mlp.experts.fuse_shared_expert(
+                            layer.mlp.shared_experts)
+                        layer.mlp.shared_experts = None
